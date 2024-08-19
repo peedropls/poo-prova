f25b1f6b7d3f6fc9a0533c9a08a5ae4b014a7767
João Pedro Pinheiro <pedro.severo@escolar.ifrn.edu.br>
2024-08-19 10:39:43 -0300

    questão 6

---
+++ questao6.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("digite sua idade:");
+int a = int.Parse(Console.ReadLine());
+if (a>18){
+    Console.WriteLine("você está apto(a) a dirigir e a votar!");
+}else{
+Console.WriteLine("você não está apto(a) a dirigir e a votar!");
+}
