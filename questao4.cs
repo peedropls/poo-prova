67d7e0acd237e8dc48235c39731609eee4938ac7
João Pedro Pinheiro <pedro.severo@escolar.ifrn.edu.br>
2024-08-19 10:35:46 -0300

    questão 4

---
+++ questao4.cs
@@ -0,0 +1,15 @@
+Console.WriteLine("digite um número:");
+int a = int.Parse(Console.ReadLine());
+if ( a>0  ){
+Console.WriteLine($"seu número é positivo!");
+}
+else if ( a < 0){ 
+    Console.WriteLine($"seu número é negativo!");
+}
+
+
+else{
+
+
+    Console.WriteLine($"seu número é igual 0!");
+}
