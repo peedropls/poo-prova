1c7dddf71570e949f70b82033fc4a9536946a2d9
João Pedro Pinheiro <pedro.severo@escolar.ifrn.edu.br>
2024-08-19 10:28:52 -0300

    questão 2

---
+++ questao2.cs
@@ -0,0 +1,12 @@
+Console.WriteLine("digite dois valores a e b:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int adc = a + b;
+Console.WriteLine($"a + b = {adc}");
+int sub = a - b;
+Console.WriteLine($"a - b = {sub}");
+int mult = a * b;
+Console.WriteLine($"a * b = {mult}");
+int div = a / b;
+Console.WriteLine($"a/b = {div}");
